4cf0958864c36b99da95a63bd32e402ef0df8510
JulioDinizN <juliocesardiniznogueira@gmail.com>
2021-11-04 19:45:34 -0300

    Challenge completed with propDrilling

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,47 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+import { SideBar } from "./SideBar";
+import { GenreResponseProps, MovieProps } from "../App";
+
+interface ContentProps {
+  selectedGenre: GenreResponseProps;
+  movies: MovieProps[];
+  handleClickButton(id: number): void;
+  selectedGenreId: number;
+}
+
+export function Content({
+  selectedGenre,
+  movies,
+  handleClickButton,
+  selectedGenreId,
+}: ContentProps) {
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <SideBar
+        handleClickButton={handleClickButton}
+        selectedGenreId={selectedGenreId}
+      />
+      <div className="container">
+        <header>
+          <span className="category">
+            Categoria:<span> {selectedGenre.title}</span>
+          </span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map((movie: any) => (
+              <MovieCard
+                key={movie.imdbID}
+                title={movie.Title}
+                poster={movie.Poster}
+                runtime={movie.Runtime}
+                rating={movie.Ratings[0].Value}
+              />
+            ))}
+          </div>
+        </main>
+      </div>
+    </div>
+  ); // Complete aqui
 }
